234618c24890b28f41d873d489ed1946ad6091b5
root <patrickalin@gmail.com>
2018-01-17 15:55:10 +0100

    last commit replace by prometheus2.0

---
+++ menu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SERVICE="monitoring"
+IMAGE="$SERVICE-image"
+
+OPTION=$(whiptail --title $SERVICE --menu "Choose your option" 15 60 4 \
+"1" "Build $SERVICE" \
+"2" "(Re)Start service $SERVICE" \
+"3" "Stop service $SERVICE" 3>&1 1>&2 2>&3)
+ 
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $OPTION
+else
+    echo "You chose Cancel."
+fi
+
+case "$OPTION" in
+
+1)  cd $IMAGE
+    docker build -t $IMAGE .
+    docker tag $IMAGE registry-srv.services.alin.be/$IMAGE
+    ;;
+2)  docker stack remove  $SERVICE
+    sleep 3
+    docker stack deploy --compose-file docker-compose.yml $SERVICE
+    ;;
+3)  docker stack remove  $SERVICE
+    ;;
+esac
